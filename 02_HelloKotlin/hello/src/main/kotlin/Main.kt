36c61bdfca3e849e38f929e99cd3cea1e9089a25
Boris <botri@yandex.ru>
2022-09-05 20:28:44 +0300

    я все написал и все проверил, но я не пойму правильно ли я работаю с GITLAB и пока ничего не понимаю

---
+++ 02_HelloKotlin/hello/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Hi my name is Boris")
+    println("I was very many years ago )) ")
+    print('I' + " ")
+    print("want to be" + ' ' + "a ")
+    print("")
+    print("programmer")
+
+}
