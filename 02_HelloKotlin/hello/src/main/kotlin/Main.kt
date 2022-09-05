b9eca026b5f44ae621ac23219d535a3fd0a549de
Boris <botri@yandex.ru>
2022-09-05 21:23:03 +0300

    опять что то исправил и опять отправляю я туповат )

--- 02_HelloKotlin/hello/src/main/kotlin/Main.kt
+++ 02_HelloKotlin/hello/src/main/kotlin/Main.kt
@@ -4,7 +4,8 @@ fun main() {
     println("I was very many years ago )) ") // Мне 38 лет
     /*  Я не увидел что нужно
     оставить многострочный коментарий
-    хотел написть println(38) */
+    хотел написть
+    println(38) */
 
     print('I' + " ")
     print("want to be" + ' ' + "a ")
