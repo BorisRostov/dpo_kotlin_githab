44483662704acb0b5e88d10c54e8cb5e1ec396b6
Boris <botri@yandex.ru>
2022-09-08 17:51:30 +0300

    Ох уж это гит лаб все воюю с ним)

--- 04_Functions/fun/src/main/kotlin/Main.kt
+++ 04_Functions/fun/src/main/kotlin/Main.kt
@@ -1,18 +1,22 @@
 fun main() {
-var str = "F2p)vy233{0->c}ttelciFc"
-var str1 = str.take(str.length/2)      // Разделить сообщение пополам.
-var str2 = str.takeLast(str.length/2)   // Разделить сообщение пополам.
-
-str1 = str1.map {char -> char + 1}.joinToString("")
-
-str1.replace('5', 's').replace('4', 'u')
-
-str1 = str1.map {char -> char - 3}.joinToString("")
-str1 = str1.replace("0", "o")
-
-str2 = str2.reversed().map {char -> char - 4}.joinToString("")
-str2 = str2.replace('_', ' ')
-str = str1 + str2
-str
-println(str)
+    var str = "F2p)v\"y233{0->c}ttelciFc"
+    var str1 = str.take(str.length/2)      // Разделить сообщение пополам.
+    var str2 = str.takeLast(str.length/2) // Разделить сообщение пополам.
+    str = first(str1) + second(str2)
+    println(str)
+}
+fun first(s:String): String {
+    var result: String
+    result = s.map {char -> char + 1}.joinToString("")
+        .replace('5', 's')
+        .replace('4', 'u')
+        .map {char -> char - 3}.joinToString("")
+        .replace("0", "o")
+    return result
+}
+fun second(s:String):String{
+    var result: String
+    result = s.reversed().map {char -> char - 4}.joinToString("")
+    result = result.replace('_', ' ')
+    return result
 }
